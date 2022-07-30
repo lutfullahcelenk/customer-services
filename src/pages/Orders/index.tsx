99fd7fe8650a1560033cbeb90966a71fd1ae6e0e
Lütfullah ÇELENK <81390201+lutfullahcelenk@users.noreply.github.com>
2022-07-30 19:52:50 +0300

    order detail page is completed

--- src/pages/Orders/index.tsx
+++ src/pages/Orders/index.tsx
@@ -1,5 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+//components
+import Filter from "../../components/Filter";
 import Sidebar from "../../components/Sidebar";
+//constants
+import { orders } from "../../constants/orders";
+import useResponsive from "../../hooks/useMediaQuery";
 
 type IOrders = {
   isOpen: boolean;
@@ -7,12 +13,71 @@ type IOrders = {
 };
 
 const Orders = ({ isOpen, setIsOpen }: IOrders) => {
+  const navigate = useNavigate();
+  const { xs } = useResponsive();
+  const [orderName, setOrderName] = useState("");
+
+  // filter function
+  const handleFilter = (event: any) => {
+    setOrderName(event.target.value);
+  };
+
+  // filtered orders
+  const filteredOrders = orders.filter(
+    (order) => order?.detail === orderName || orderName === ""
+  );
+
   return (
     <div className="flex h-screen gradient">
       <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
 
-      <div>
-        
+      <div className="w-full justify-start flex flex-col p-2 shadow-md sm:rounded-lg">
+        <Filter handleFilter={handleFilter} orderName={orderName} />
+
+        <div className="overflow-x-auto ">
+          <table className="w-full mt-6 text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-gray-700 capitalize bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                <th scope="col" className="py-3 px-6">
+                  Müşteri Adı
+                </th>
+                <th scope="col" className="py-3 px-6">
+                  Sipariş Tarihi
+                </th>
+                <th scope="col" className="py-3 px-6">
+                  Sipariş Tutarı
+                </th>
+                <th scope="col" className="py-3 px-6">
+                  Sipariş Detayı
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredOrders.map((order) => (
+                <tr key={order.id} className="bg-white border-b">
+                  <th
+                    scope="row"
+                    className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap"
+                  >
+                    {order.customer}
+                  </th>
+                  <td className="py-4 px-6">{order.date}</td>
+                  <td className="py-4 px-6">{order.cost} TL</td>
+                  <td className="py-4 px-6 flex justify-between">
+                    {order.detail}
+                    <button
+                      type="button"
+                      className="border bg-indigo-700 text-white px-2 ml-4 py-1 rounded-lg"
+                      onClick={() => navigate(`/orders/${order.id}`)}
+                    >
+                      {xs ? "Detay" : "Sipariş Detayı"}
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
